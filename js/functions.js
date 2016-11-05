c84764b7c21a1f42b2922d25152326245c13d11d
CecyNeria <cecith-06@hotmail.com>
2016-11-04 18:33:13 -0600

    add:las funciones y las columnas

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-	$("#enviar").click(function(){
-		alert($("#usr").val());
+	$("#enviarhola").click(function(){
+		alert($("#hola").val());
+		});
+	$("#enviaradios").click(function(){
+		alert($("#adios").val());
+		});
+	$("#enviarteamo").click(function(){
+		alert($("#teamo").val());
+		});
+	$("#enviarrespuesta").click(function(){
+		if ($("#respuesta").val() == "hola"){
+			alert("Hola, ¿Cómo estás?");}
+		else if ($("#respuesta").val() == "adios"){
+			alert("Adiós,¡Que te vaya bien!");}
+		else if ($("#respuesta").val() == "te amo"){
+			alert("¡Yo tambien te amo <3!");}
 		});
 });
